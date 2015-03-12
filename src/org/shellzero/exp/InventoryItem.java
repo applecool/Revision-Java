3592dc3df18e906cda63936664d61b0a22555e74
applecool <kmitharsha@gmail.com>
2015-03-12 01:32:49 -0500

    added InventoryItem

---
+++ src/org/shellzero/exp/InventoryItem.java
@@ -0,0 +1,35 @@
+package org.shellzero.exp;
+
+enum ItemType{POTION, RING, ARMOUR};
+
+public class InventoryItem {
+	
+	private ItemType type;
+	private String name;
+	
+	public InventoryItem(ItemType type, String name) {
+		super();
+		this.type = type;
+		this.name = name;
+	}
+
+	public ItemType getType() {
+		return type;
+	}
+
+	public void setType(ItemType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
